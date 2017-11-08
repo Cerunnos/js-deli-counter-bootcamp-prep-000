59a2d162b496813cf35a1344a65f73e53812f7ef
declanm.mulligan@gmail.com Mulligan <declanm.mulligan@gmail.com>
2017-11-08 17:06:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function currentLine(line){
   }
   else{
     var placeInLine=[]
-    for (var i=0,1=line.length;i<1;i++)
+    for (let i=0,1=line.length;i<1;i++)
     return `The line is currently: ${line.length}. ${line},`
   }
 }
